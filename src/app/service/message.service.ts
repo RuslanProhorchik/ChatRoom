be96aceacba7e4a659982931f2c6c59ab14d51ee
Ruslan <prohorchikrv@gmail.com>
2019-12-12 17:03:48 +0300

    Add support for id message

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Message } from '../models/message';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,14 @@ export class MessageService {
   messages: Observable<Message[]>;
 
   constructor(public afs:AngularFirestore) { 
-    this.messages = this.afs.collection('messages').valueChanges();
+    //this.messages = this.afs.collection('messages').valueChanges();
+    this.messages = this.afs.collection('messages').snapshotChanges().pipe(map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Message;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    }));
   }
 
   public getMessages() {
