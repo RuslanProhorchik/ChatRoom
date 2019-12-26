3af4cfa8294093e567963bd978eb5978921f19e4
Ruslan <prohorchikrv@gmail.com>
2019-12-26 15:45:43 +0300

    Add owner uid to message

--- src/app/components/message/create/message-create.component.ts
+++ src/app/components/message/create/message-create.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Message } from 'src/app/models/message';
 import { MessageService } from 'src/app/service/message.service';
+import { AuthService } from 'src/app/service/auth.service';
+import { IUserDetail } from 'src/app/models';
+import { UserDetailService } from 'src/app/service/user-detail.service';
 
 @Component({
   selector: 'app-message-create',
@@ -23,19 +26,36 @@ export class MessageCreateComponent implements OnInit {
 
   get messagesUid(): string { return this._messagesUid; }
 
+  activeUser: firebase.User;  
+  activeUserDetail: IUserDetail;
+
   message: Message = {    
     text: ''    
   };
 
-  constructor(private messageService: MessageService) {
+  constructor(
+    private messageService: MessageService,
+    private auth: AuthService, 
+    private uds: UserDetailService) {
    }
 
   public ngOnInit() {
+    this.auth.getUserState()
+    .subscribe(user => {
+      this.activeUser = user;
+
+      console.log(user.uid);
+      this.uds.getUserDetail(user.uid).subscribe(detail => {
+        this.activeUserDetail = detail;        
+        //console.log(this.activeUserDetail);
+      });            
+    });    
   }
 
   public onSubmit() {
     if(this.message.text != '') {
       this.message.createdAt = new Date(Date.now());
+      this.message.ownerUid = this.activeUserDetail.uid;
 
       this.addMessage(this.message);
       this.clearFields();      
